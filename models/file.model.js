ac702f121ec4459449ffcc344faf5946de102890
Tarek Jaman Labu <tarekjamanlabu515@gmail.com>
2025-03-22 14:59:26 +0600

    modify(model): Add size field in File Schema

--- models/file.model.js
+++ models/file.model.js
@@ -1,19 +1,23 @@
 import mongoose from "mongoose";
 
 const fileSchema = new mongoose.Schema({
-    filename: {
-        type: String,
-        required: [true, "File name is required."]
-    },
-    url: {
-        type: String,
-        required: [true, "File url is required."]
-    },
-    uploadedAt: {
-        type: Date,
-        default: Date.now
-    }
-})
+  filename: {
+    type: String,
+    required: [true, "File name is required."],
+  },
+  url: {
+    type: String,
+    required: [true, "File url is required."],
+  },
+  size: {
+    type: Number,
+    required: [true, "File size is required."],
+  },
+  uploadedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 const File = mongoose.model("File", fileSchema);
 export default File;
