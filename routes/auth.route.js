0126dfb06260e0077bcd7c702184aa3a31581c63
Tarek Jaman Labu <tarekjamanlabu515@gmail.com>
2025-03-22 14:42:13 +0600

    feat: Add Google Strategy with proper setup

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,15 +1,22 @@
 import express from "express";
-import {googleAuth, login, logout, signup} from "../controllers/auth.controller.js";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 import passport from "passport";
 
-const router = express.Router()
+const router = express.Router();
 
-router.post("/signup", signup)
-router.post("/login", login)
-router.post("/google", googleAuth)
-router.post("/google/callback", passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
-    res.redirect('/dashboard');
-})
-router.get("/logout", logout)
+router.post("/signup", signup);
+router.post("/login", login);
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] }),
+);
+router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    res.redirect("/dashboard");
+  },
+);
+router.get("/logout", logout);
 
-export default router
+export default router;
--- src/app.js
+++ src/app.js
@@ -1,37 +1,39 @@
-import express from 'express'
-import cors from 'cors'
-import session from 'express-session'
-import passport from "passport";
-import { config } from 'dotenv'
-import "../config/passport.js"
+import express from "express";
+import cors from "cors";
+import session from "express-session";
+import { config } from "dotenv";
+import passport from "../config/passport.js";
 import connectDB from "../lib/db.js";
 import authRoute from "../routes/auth.route.js";
 
 config();
 
-const app = express()
+const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Session Middleware
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: false
-}));
+    saveUninitialized: false,
+  }),
+);
 
 // Initialize Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.get("/", (req, res) => {
-    res.send("Welcome to the AI based document summarizer API.");
-})
+  res.send("Welcome to the AI based document summarizer API.");
+});
 
 app.use("/api/v1/auth", authRoute);
 
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, async () => {
-    console.log(`Server is running on PORT: ${PORT}`);
-    await connectDB();
-})
+  console.log(`Server is running on PORT: ${PORT}`);
+  await connectDB();
+});
